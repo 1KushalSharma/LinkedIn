fdc0ff66f83da00af6454453f8bdab22f8f43f93
Aditya Bhandarkar <71058555+a-bhandarkar@users.noreply.github.com>
2022-01-08 12:59:02 +0530

    After "Send E-mail" feature

---
+++ Login.java
@@ -0,0 +1,6 @@
+package com.LinkedInApp;
+
+public interface Login {
+	public void giveAccountDetails();
+	public String getLogin();
+}
---
+++ UserDetails.java
@@ -0,0 +1,36 @@
+package com.LinkedInApp;
+
+public class UserDetails {
+	private String Name;
+	private String Role;
+	private String Email;
+	private String password;
+	
+	public String getName() {
+		return Name;
+	}
+	public void setName(String name) {
+		Name = name;
+	}
+	public String getRole() {
+		return Role;
+	}
+	public void setRole(String role) {
+		Role = role;
+	}
+	
+	public String getEmail() {
+		return Email;
+	}
+	public void setEmail(String email) {
+		Email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+
+}
